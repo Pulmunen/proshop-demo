5bf1a5cb742ce7cc7a88a62125d705e257584714
Pulmunen <github.com.mustiness480@passmail.net>
2024-09-25 09:08:49 +0300

    first commit for frontend and backend

--- frontend/src/screens/ProductScreen.js
+++ frontend/src/screens/ProductScreen.js
@@ -4,13 +4,22 @@ import {Link} from 'react-router-dom'
 import {Row, Col, Image, ListGroup, Card, Button} from 'react-bootstrap'
 import Rating from '../components/Rating'
 
-import products from '../products'
+import axios from 'axios'
+import {useEffect, useState} from 'react'
 
 const ProductScreen = () => {
+
+	const [product, setProduct] = useState({})
+
+	const {id:productId} = useParams()
 	
-	const {id} = useParams()
-	const product = products.find((p) => p._id === id)
-	console.log(product)
+	useEffect(()=>{
+		const fetchProduct = async ()=>{
+			const {data} = await axios.get(`/api/products/${productId}`)
+			setProduct(data)
+		}
+		fetchProduct()
+	},[productId])
   return (
 	<>
 		<Link className="btn btn-light my-3" to="/">
